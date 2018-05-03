3c1580fac90b7a667c0072f6fa76459f571822f5
flutterbrony <antoinemz@gmail.com>
2018-05-03 10:38:24 +0200

    tape dans l'api

--- Client/client.js
+++ Client/client.js
@@ -1,21 +1,20 @@
 #!/usr/bin/env node
 const program = require('commander')
 var express = require('express');
-var toto;
+var axios = require('axios')
 
 var cli = express();
+var http = "http://localhost:3000/"
 
 // Configuration des paramètres attendus
 program
     .version('1.0.0')
     .option('-c, --create', 'Create a new note')
     .option('-l, --list', 'List all notes')
-    .option('-e, --edit [id]', 'edit note by id')
     .option('-x, --delete [id]', 'Delete note by id')
     .option('-p, --priority [priority]', 'Define importance of the note')
     .option('-t, --content [content]', 'write your note here')
     .option('-s, --stopDate [date]', 'Define an expiration date')
-    .option('-d, --isDone [bool]', 'define is task is done or not')
     .option('-n, --name [name]', 'define the name of the note')
 
 // On parse (convertit en format utilisable) les options
@@ -26,62 +25,28 @@ program
 // Maintenant on peut les utiliser
 if (program.create) {
     console.log('Create note !')
+    var post = {"name" : `${program.name}`,
+                "content" : `${program.content}`,
+                "priority" : `${program.priority}`,
+                "expiration_date" : `${program.stopDate}`};
     if (program.name && program.content) {
-        console.log(`name ${program.name}!`)
-        console.log(`content ${program.content}!`)
+        axios.post(http + post);
     }
-    if (program.priority) {
-        console.log(`priority ${program.priority}!`)
-    }
-    /*if (program.content) {
-        console.log(`content ${program.content}!`)
-    }*/
-    if (program.stopDate) {
-        console.log(`stop date ${program.stopDate}!`)
-    }
-    if (program.isDone) {
-        console.log(`is done ${program.isDone}!`)
+
+    else {
+        console.log("vous n'avez pas défini name ou content.");
+        console.log("Voir client -h pour l'aide");
     }
 }
+
 else if (program.list) {
     console.log('List!')
 }
-else if (program.edit) {
-    console.log(`edit note ${program.edit}!`)
-    if (program.name) {
-        console.log(`name ${program.name}!`)
-    }
-    if (program.priority) {
-        console.log(`priority ${program.priority}!`)
-    }
-    if (program.content) {
-        console.log(`content ${program.content}!`)
-    }
-    if (program.stopDate) {
-        console.log(`stop date ${program.stopDate}!`)
-    }
-    if (program.isDone) {
-        console.log(`is done ${program.isDone}!`)
-    }
-}
+
 else if (program.delete) {
     console.log(`deleted note ${program.delete}!`)
 }
-else if (program.name) {
-    console.log(`name ${program.delete}!`)
-}
-else if (program.priority) {
-    console.log(`priority ${program.delete}!`)
-}
-else if (program.content) {
-    console.log(`content ${program.delete}!`)
-}
-else if (program.stop-date) {
-    console.log(`stop date ${program.delete}!`)
-}
-else if (program.is-done) {
-    console.log(`is done ${program.delete}!`)
-}
+
 else
     {program.help()
 }
